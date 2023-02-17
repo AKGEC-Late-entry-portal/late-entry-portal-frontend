c948faabe01c07828581dcccb9fdf344861365b7
rahulXbarnwal <rahulbarnwalofficial@gmail.com>
2023-02-17 16:19:44 +0530

    ProtectedRoute Implemented

--- src/util/ProtectedRoute.js
+++ src/util/ProtectedRoute.js
@@ -3,25 +3,19 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
-    const navigate = useNavigate();
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const checkUserToken = () => {
-        const userToken = localStorage.getItem('token');
-        if (!userToken || userToken === 'undefined') {
-            setIsLoggedIn(false);
-            return navigate('/login');
-        }
-        setIsLoggedIn(true);
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const checkUserToken = () => {
+    const userToken = localStorage.getItem("token");
+    if (!userToken || userToken === "undefined") {
+      setIsLoggedIn(false);
+      return navigate("/login");
     }
-    useEffect(() => {
-            checkUserToken();
-        }, [isLoggedIn]);
-    return (
-        <React.Fragment>
-            {
-                isLoggedIn ? props.children : null
-            }
-        </React.Fragment>
-    );
-}
+    setIsLoggedIn(true);
+  };
+  useEffect(() => {
+    checkUserToken();
+  }, [isLoggedIn]);
+  return <React.Fragment>{isLoggedIn ? props.children : null}</React.Fragment>;
+};
 export default ProtectedRoute;
